e32d876070c64eb2f1591b49f896c8183c6038f2
st-0403-jp <sato252011@gmail.com>
2016-03-15 00:05:10 +0900

    consoleのテキストを変更

--- js/test.js
+++ js/test.js
@@ -2,7 +2,7 @@
 
 (function () {
   var test = function (ele) {
-    console.log(ele.innerText);
+    console.log("h1タグの中身は：" + ele.innerText);
   };
   var h1Tag = document.querySelector('h1');
   test(h1Tag);
